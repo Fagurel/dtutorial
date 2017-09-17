d77027375df56aeaa8f7c78dd60a9069d0f2c40d
andriydzhrvts <krash2233@gmail.com>
2017-09-17 17:12:14 +0300

    5 commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yg!xg=%75)6e*kr^t52p6@yw)d)!b07icufhh1ajco+a@tw2h)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
